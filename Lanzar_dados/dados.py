0bf4617c315ba17932dd96eb21ba2782cf95e88e
Cayalan <carlos2224648@correo.uis.edu.co>
2022-10-13 09:13:59 -0500

    subir

--- Lanzar_dados/dados.py
+++ Lanzar_dados/dados.py
@@ -2,29 +2,29 @@ print("Lamzamiento de N dados")
 # Input
 N=int(input("Cuantas veces vas a lanzar el dado: "))
 # Processing
-c1= 0
-c2= 0
-c3= 0
-c4= 0
-c5= 0
-c6= 0
+c1= ""
+c2= ""
+c3= ""
+c4= ""
+c5= ""
+c6= ""
 
 import random
 
 for j in range(N):
     j = random.randint(1,6)
     if j == 1:
-        c1=c1+ 1
+        c1=c1+ "*"
     elif j == 2:
-        c2= c2 +1 
+        c2= c2 +"*" 
     elif j == 3:
-        c3= c3 + 1 
+        c3= c3 + "*" 
     elif j == 4:
-        c4=c4 + 1  
+        c4=c4 + "*" 
     elif j == 5:
-        c5= c5 + 1
+        c5= c5 + "*"
     elif j == 6:
-        c6= c6 + 1 
+        c6= c6 + "*"
        
 print("Las veces que te salio 1 fue: ",c1)
 print("Las veces que te salio 2 fue: ",c2)
